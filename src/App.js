dffb08b8572f756e474ec6466b54c5630198d299
deltagare <olusanyaabayomi@yahoo.com>
2021-04-30 10:46:48 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Chuck from './components/Chuck';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Chuck />
+			</header>
+		</div>
+	);
+}
+
+export default App;
